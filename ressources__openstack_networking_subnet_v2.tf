40ac2da38ba870bdc22ebd43fbe93060ad555266
Tristan Le toullec <tristan.letoullec@cnrs.fr>
2023-12-20 13:40:46 +0100

    Add DNS to subnet

--- ressources__openstack_networking_subnet_v2.tf
+++ ressources__openstack_networking_subnet_v2.tf
@@ -3,4 +3,5 @@ resource openstack_networking_subnet_v2 private {
   network_id = openstack_networking_network_v2.private.id
   cidr       = var.PRIVATE_SUBNET_CIDR
   ip_version = 4
+  dns_nameservers = ["8.8.8.8", "1.1.1.1"]
 }
