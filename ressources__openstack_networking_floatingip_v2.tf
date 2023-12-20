ae9e9055e0a87bccd5bb0a4a29008562e976ebc0
Tristan Le toullec <tristan.letoullec@cnrs.fr>
2023-12-20 13:41:08 +0100

    Add tags and desc to fip

--- ressources__openstack_networking_floatingip_v2.tf
+++ ressources__openstack_networking_floatingip_v2.tf
@@ -1,3 +1,5 @@
 resource openstack_networking_floatingip_v2 jupyterhub {
   pool = data.openstack_networking_network_v2.public.name
+  description = "hub.jupyter.cloud"
+  tags = ["fqdn=hub.jupyter.cloud"]
 }
